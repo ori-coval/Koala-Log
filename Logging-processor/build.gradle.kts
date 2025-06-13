9823ce4a9a04319779077bf0fadc5066db82060e
kully <oricoval@gmail.com>
2025-06-13 16:47:54 +0300

    added all projects

---
+++ Logging-processor/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("java")
+}
+
+
+dependencies {
+    implementation("com.squareup:javapoet:1.13.0")
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
